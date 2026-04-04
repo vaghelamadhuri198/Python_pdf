611e9cc820d2000098d1ca138be196211a2c04bf
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-04 10:42:53 +0530

    Arithmetic operater.java

---
+++ Arithmetic operater.java
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+print(a + b)  # Addition → 13
+print(a - b)  # Subtraction → 7
+print(a * b)  # Multiplication → 30
+print(a / b)  # Division → 3.33
+print(a % b)  # Modulus → 1
+print(a ** b) # Power → 1000
+print(a // b) # Floor Division → 3
