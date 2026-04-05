00de2aa632bc28bf2bccd0ac9c3d2c559a6766ce
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-05 10:22:05 +0530

    String method.java

---
+++ String method.java
@@ -0,0 +1,18 @@
+str = "i am engineer.";
+
+print(str.endswith("er."));
+#returns true if string ends with substr
+
+
+print(str.capitalize());
+#capitalizes 1st char
+
+
+print(str.replace("i","you"));
+#replaces all occurrences of old with new
+
+print(str.find("am"));
+#returns 1st index of 1st occurrence
+
+
+print(str.count("am")) ;#counts the occurrence of substr in string
