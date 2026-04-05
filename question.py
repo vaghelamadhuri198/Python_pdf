7d41b6aa7fde36908b658c810096417815e840c6
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-05 11:37:12 +0530

    question.py

---
+++ question.py
@@ -0,0 +1,9 @@
+#WAP to input user’s first name & print its length.
+
+name ="vaghela_madhuri";
+print(len(name));
+
+#WAP to find the occurrence of ‘$’ in a String.
+
+name ="& vaghela_ & madhuri & ";
+print(name.count("&"));
