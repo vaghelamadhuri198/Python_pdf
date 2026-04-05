7c95cd915833fbef83d55b5599fdb581d0edd944
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-05 11:35:40 +0530

    Multiple numbers.py

---
+++ Multiple numbers.py
@@ -0,0 +1,7 @@
+#if a number is a multiple of 7 or not.
+num = int(input("Enter a number: "))
+
+if num % 7 == 0:
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
