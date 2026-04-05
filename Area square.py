90ba5ce08c1309cf3faa81b7f577ab88fec309f1
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-05 11:33:34 +0530

    Area square.py

---
+++ Area square.py
@@ -0,0 +1,10 @@
+#WAP to input side of a square & print its area. in python
+
+# Input side of square
+side = float(input("Enter side of square: "))
+
+# Calculate area
+area = side * side
+
+# Print result
+print("Area of square is:", area)
