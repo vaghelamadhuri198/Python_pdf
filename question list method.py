a8059cd7707f6ef32d7049f109171cce6bc58f70
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-06 13:48:24 +0530

    question list method.py

---
+++ question list method.py
@@ -0,0 +1,8 @@
+#Store the above values in a list & sort them from “A” to “D”
+
+# Store grades in a list
+grades = ["A", "B", "A", "C", "A", "B", "D"]
+
+grades.sort()
+
+print("Sorted grades:", grades)
