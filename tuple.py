3ade25d801a19e763b6ce6a00fdbb048dd6b4c91
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-06 13:50:04 +0530

    tuple.py
    #A built-in data type that lets us create immutable sequences of values
    
    #A tuple is a collection of elements that are:
    #Ordered
    #Immutable (cannot be changed after creation)
    #Allows duplicate values

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tup = (87, 64, 33, 95, 76) #tup[0], tup[1]..
+print(tup)
+print(type(tup))
+
+#tup[0] = 43 #NOT allowed in python
+
+tup1 = ( )
+
+print(tup1)
+
+tup2 = (1,)
+tup2 = (1)
+print(type(tup2))
+
+tup3 = ( 1, 2, 3 )
+print(type(tup3))
+
