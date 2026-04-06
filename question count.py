c62c1dfce7b635f6d2e5511a6c88cf91dea02d06
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-06 13:54:15 +0530

    question count.py

---
+++ question count.py
@@ -0,0 +1,12 @@
+#WAP to count the number of students with the “A” grade in the following tuple. in python
+
+# Tuple of student grades
+grades = ("A", "B", "A", "C", "A", "B")
+
+print(grades.count("A"))
+
+# Count number of "A" grades
+#count = grades.count("A")
+
+# Display result
+#print("Number of students with A grade:", count)
