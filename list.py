a2266be13dc4d248f1b8ed019a79030890a19e64
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-06 13:42:10 +0530

    list.py
    #A built-in data type that stores set of values
    #It can store elements of different types (integer, float, string, etc.)
    
    #A list is a data type used to store multiple items in a single variable 📦.
    
    #Ordered
    #Changeable (mutable)
    #Allows duplicate values

---
+++ list.py
@@ -0,0 +1,25 @@
+marks = [87, 64, 33, 95, 76]
+print(marks)
+print(type(marks))
+
+student = ["Karan", 85,
+Delhi"]
+student [1] = "meera"
+print(student)
+print(len(student))
+
+#list slicing
+marks = [87, 64, 33, 95, 76]
+print(marks)
+
+print(marks[1:4]) #is [64, 33, 95]
+
+print(marks[ : 4 ]) #is same as marks[ 0 : 4]
+
+print(marks[ 1 : ])#is same as marks[ 1 : len(marks) ]
+
+print(marks[ -3 : -1 ]) #is [33, 95]
+
+
+
+
