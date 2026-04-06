25eb79338ff3fa25cd979b1ec5ad9e5e9d0fbc4a
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-06 13:43:43 +0530

    list method.py

---
+++ list method.py
@@ -0,0 +1,29 @@
+list = [2, 1, 3]
+#list.append(4)
+#print(list) #adds one element at the end [2, 1, 3, 4]
+
+#list.sort( ) #sorts in ascending order
+
+#print(list)  #[1, 2, 3, 4]
+
+#list.sort( reverse=True ) #sorts in descending order
+ 
+#print(list)
+
+list.reverse( ) #reverses list
+[3, 1, 2]
+print(list)
+
+list.insert( 3, 2 ) #insert element at index
+print(list)  #[3, 1, 2, 2]
+
+list.remove(1) #removes first occurrence of element
+[3, 2 , 2]
+print(list)
+
+list.pop( 2 ) #removes element at idx
+
+#[3 , 2 ]
+
+print(list)
+
